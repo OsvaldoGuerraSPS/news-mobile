f924f79d1094a18de2b17e110171529a55102456
Osvaldo Guerra <oguerral@spsolutions.com.mx>
2024-10-17 11:56:04 -0600

    Home Screen

---
+++ src/config/theme/global-styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    title: {
+        fontSize: 25,
+        fontWeight: 'semibold',
+        color: 'black',
+    }
+})
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native"
+import React, { useEffect, useState } from "react"
+import { Text, FlatList } from "react-native"
+import { getNewsEverything } from "../../../data/api/actions/get.news.everything";
+import { News } from "../../../domain/model/news";
+import { MySafeArea } from "../../components/ui/MySafeArea";
+import { NewsItem } from "../../components/news/NewsItem";
+import { globalStyles } from "../../../config/theme/global-styles";
 
 export const HomeScreen = () => {
+
+    const [news, setNews] = useState<News[]>([]);
+
+    useEffect(() => {
+        getNews()
+    },[]);
+
+    const getNews = async () => {
+        const news = await getNewsEverything();
+        setNews(news);
+    }
+
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <MySafeArea>
+            <Text style={globalStyles.title}>Noticias del Mundo</Text>
+            <FlatList
+                data={news}
+                renderItem={({item}) => <NewsItem news={item}/>}
+                keyExtractor={(item) => item.title}
+            />
+        </MySafeArea>
     )
 }
