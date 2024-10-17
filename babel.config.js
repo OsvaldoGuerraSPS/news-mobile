a73c56a44ea1def65cfba47705732a74e388d26e
Osvaldo Guerra <oguerral@spsolutions.com.mx>
2024-10-16 20:25:22 -0600

    config env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const { Platform } = require("react-native");
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  Platform: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      }
+    ]
+  ]
 };
